33bf9adc084c572a219bf1291aa3e1f782d4544b
satavan79 <69298016+satavan79@users.noreply.github.com>
2023-03-05 11:12:22 +0700

    Update odoo_install.sh

--- odoo_install.sh
+++ odoo_install.sh
@@ -37,7 +37,7 @@ OE_SUPERADMIN="admin"
 GENERATE_RANDOM_PASSWORD="True"
 OE_CONFIG="${OE_USER}-server"
 # Set the website name
-WEBSITE_NAME="runbot.iapp.vn"
+WEBSITE_NAME="erpcloud.vn"
 # Set the default Odoo longpolling port (you still have to use -c /etc/odoo-server.conf for example to use this.)
 LONGPOLLING_PORT="8072"
 # Set to "True" to install certbot and have ssl enabled, "False" to use http
